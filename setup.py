d854cd454d7ad3b45e7671370f9014b66b3a2dc4
fluidmilan <milan.patel@fluid-it.com>
2023-03-13 17:46:19 +0000

    commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+import setuptools
+
+setuptools.setup(
+    name='Fluid_Common_Code',
+    version='0.0.1',
+    author='Milan Patel',
+    author_email='milan.patel@fluid-it.com',
+    description='Installation of Package',
+    url='https://github.com/fluidmilan/Fluid_Common_Code.git',
+    license='unlicense',
+    packages=['Fluid_Common_Code']
+)
