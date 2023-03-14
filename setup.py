d5f9d29612cc45a793261ce69d90f9902eb47305
fluidmilan <milan.patel@fluid-it.com>
2023-03-14 10:36:16 +0000

    renaming package

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 import setuptools
 
 setuptools.setup(
-    name='Fluid_Common_Code',
+    name='message_logging',
     version='0.0.1',
     author='Milan Patel',
     author_email='milan.patel@fluid-it.com',
     description='Installation of Package',
     url='https://github.com/fluidmilan/Fluid_Common_Code.git',
     license='unlicense',
-    packages=['Fluid_Common_Code']
+    packages=['']
 )
